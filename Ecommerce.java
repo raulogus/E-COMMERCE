9b0a52614552eceae8690deec016df6e1257810e
Raulogus <raulogus2@gmail.com>
2020-11-11 10:58:32 -0300

    Ecomerce

---
+++ Ecommerce.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Ecommerce 
+  {
+	public static void main(String[] args) 
+	  {
+		Scanner leia = new Scanner(System.in);
+		int estoque=10;
+        String camisa  []= new String[estoque];
+        String jaquetas[]= new String[estoque];
+        String moletons[]= new String[estoque];
+        String regatas []= new String[estoque];
+        String shorts  []= new String[estoque];
+        String calcas  []= new String[estoque];
+        String jalecos []= new String[estoque];
+        String cintos  []= new String[estoque]; 
+        String luvas   []= new String[estoque];
+        String meias   []= new String[estoque];
+        
+     
+	  }
+  }
